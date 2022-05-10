7ab8a89b8e33379e3ed0db2b48d64818db5d278b
Raoni Lobo <raonilobo@hotmail.com>
2022-05-10 18:37:39 -0300

    Alterações Operadores

--- modulo1/operadores/template_javascript/index.js
+++ modulo1/operadores/template_javascript/index.js
@@ -6,3 +6,47 @@
 // c - true
 // d - boolean
 
+// Questão 2
+// Esqueceu de adicionar o Number para converter a String
+
+// Questão 3
+
+// let primeiroNumero = Number(prompt("Digite um numero!"))
+// let segundoNumero = Number(prompt("Digite outro numero!"))
+
+// const soma = (primeiroNumero + segundoNumero)
+
+// console.log(soma )
+
+
+// Exercício de Escrita de código
+
+// Questão 1
+
+// let idadeUsuario = Number(prompt("Qual é a sua idade?"))
+// let idadeAmigo = Number(prompt("Qual é a a idade do seu melhor amigo ?"))
+// let resultado = idadeUsuario > idadeAmigo
+// console.log( resultado, idadeUsuario - idadeAmigo )
+
+//Questão 2
+
+// let inserirNumeroPar = Number(prompt("Insira um número par!"))
+// let resultado = (inserirNumeroPar % 2)
+// console.log(resultado)
+
+// c- Sempre resto 0
+// d - Sempre resto 1
+
+
+// Questão 3
+// let idadeUsuario = Number(prompt("Qual é a sua idade?"))
+// let resultado = console.log(idadeUsuario * 12, idadeUsuario * 365, idadeUsuario * (24*365))
+
+//Questão 4
+// let primeiroNumero = Number(prompt("Diga um número?"))
+// let segundoNumero = Number(prompt("Diga outro número?"))
+// console.log(primeiroNumero > segundoNumero)
+// console.log(primeiroNumero === segundoNumero)
+// console.log((primeiroNumero % segundoNumero)===0)
+// console.log((segundoNumero % primeiroNumero)===0)
+
