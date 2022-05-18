952b5fd5cc5405d5cc53186c8893aef925e15bd9
Raoni Lobo <teste@MacBook-Pro-de-Raoni.local>
2022-05-18 16:31:15 -0300

    Atividade Lacos

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,57 @@
+// Exercícios de interpretação de código
+
+// 1.
+
+// imprimir 10. Pois o valor = 0 será somado à (+=)10
+
+// 2.
+
+// A) [19, 21, 23, 25, 27, 30]
+
+// B) Sim. const elemento = lista[i]
+
+// 3.
+
+// O resultado impresso são 4 linhas de asteriscos* de 1 à 4
+
+// Exercícios de escrita de código
+// 1. 
+
+
+
+// let quantidadeBichosEstimacao = Number(
+//   prompt("Quantos bichos de estimação você tem?")
+// );
+// let listBichos = [];
+
+
+// if (quantidadeBichosEstimacao === 0) {
+//   console.log("Que pena! Você pode adotar um pet!");
+// } else if (quantidadeBichosEstimacao > 0) {
+//   for (let i = 0; i < quantidadeBichosEstimacao; i++) {
+//     let nomesBichos = prompt("Digite os nomes do(s) seu(s) pet(s)");
+//     listBichos.push(nomesBichos);
+    
+//   }
+// }
+
+// console.log(listBichos)
+
+
+// 2.
+    let arrayOriginal = [5,10,15,20,25,30]
+    let arrayNovo = []
+    // for(let numero of arrayOriginal) { 
+    //     console.log(numero)
+    // }
+   
+    // for(let numero of arrayOriginal) { 
+    //    let divisaoNumero =  numero / 10
+    //    console.log(divisaoNumero)
+    // }
+
+    // for(let numero of arrayOriginal) {
+    //     arrayNovo.push(numero % 2 )
+    // }
+
+    // console.log(arrayNovo)
