3cb313bd0bf15e1d0bf0b35dce8696c48805b273
Raoni Lobo <teste@MacBook-Pro-de-Raoni.local>
2022-05-23 11:09:02 -0300

    desafio incompleto

--- modulo1/projeto-blackjack/Blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/Blackjack/nossoJogo.js
@@ -13,38 +13,40 @@
 
 // Sorteia uma carta. Por exemplo, o rei de ouros
 
-console.log("Boas vindas ao jogo de Blackjack!");
+// console.log("Boas vindas ao jogo de Blackjack!");
 
-let inicioJogo = confirm("Quer iniciar uma nova rodada?");
-if (inicioJogo === true) {
-  let cartaUsuario1 = comprarCarta();
-  let cartaUsuario2 = comprarCarta();
-  let cartaComputador1 = comprarCarta();
-  let cartaComputador2 = comprarCarta();
+// let inicioJogo = confirm("Quer iniciar uma nova rodada?");
+// if (inicioJogo === true) {
+//   let cartaUsuario1 = comprarCarta();
+//   let cartaUsuario2 = comprarCarta();
+//   let cartaComputador1 = comprarCarta();
+//   let cartaComputador2 = comprarCarta();
 
-  let pontosUsuario = cartaUsuario1.valor + cartaUsuario2.valor;
-  let pontosComputador = cartaComputador1.valor + cartaComputador2.valor;
+//   let pontosUsuario = cartaUsuario1.valor + cartaUsuario2.valor;
+//   let pontosComputador = cartaComputador1.valor + cartaComputador2.valor;
 
-  let impressoUsuario = cartaUsuario1.texto;
-  let impressoUsuario2 = cartaUsuario2.texto;
-  let impressoComputador = cartaComputador1.texto;
-  let impressoComputador2 = cartaComputador2.texto;
+//   let impressoUsuario = cartaUsuario1.texto;
+//   let impressoUsuario2 = cartaUsuario2.texto;
+//   let impressoComputador = cartaComputador1.texto;
+//   let impressoComputador2 = cartaComputador2.texto;
 
-  console.log(
-    `Usuário - cartas: ${impressoUsuario} ${impressoUsuario2}  - pontuação ${pontosUsuario}`
-  );
-  console.log(
-    `Computador - cartas: ${impressoComputador} ${impressoComputador2}  - pontuação ${pontosComputador}`
-  );
+//   console.log(
+//     `Usuário - cartas: ${impressoUsuario} ${impressoUsuario2}  - pontuação ${pontosUsuario}`
+//   );
+//   console.log(
+//     `Computador - cartas: ${impressoComputador} ${impressoComputador2}  - pontuação ${pontosComputador}`
+//   );
 
-  if (pontosUsuario > pontosComputador) {
-    console.log("Você ganhou!");
-  } else if (pontosUsuario < pontosComputador) {
-    console.log("Você perdeu!");
-  } else if (
-    (pontosUsuario === pontosComputador) ===
-    console.log("Você empatou!")
-  );
-} else {
-  console.log("O jogo acabou!");
-}
+//   if (pontosUsuario > pontosComputador) {
+//     console.log("Você ganhou!");
+//   } else if (pontosUsuario < pontosComputador) {
+//     console.log("Você perdeu!");
+//   } else if (
+//     (pontosUsuario === pontosComputador) ===
+//     console.log("Você empatou!")
+//   );
+// } else {
+//   console.log("O jogo acabou!");
+// }
+
+
