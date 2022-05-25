1dda49ecc9fc6442bcf4b2e4efe76375ededc0f3
Raoni Lobo <teste@MacBook-Pro-de-Raoni.local>
2022-05-25 17:09:48 -0300

    Atividade Html - CSS

---
+++ modulo1/html-sistema-loja/fixacaoJS_2.md
@@ -0,0 +1,17 @@
+```
+
+function calculaPrecoTotal(quantidade) {
+    // Escreva seu código aqui
+    let precoTotal;
+    
+  if(quantidade < 12) { 
+    precoTotal = quantidade * 1.3
+  
+  } else {
+    precoTotal = quantidade * 1
+    
+    }
+    return precoTotal
+    }
+
+    ```
--- modulo1/template_javascript/index.js
+++ modulo1/template_javascript/index.js
@@ -48,14 +48,15 @@ Questão 2
 
 // Questão 3
 
-let a = 10
-let b = 25
-let c;
-c = a
-a = b
-b = c
+// let a = 10
+// let b = 25
+// let c;
+// c = a
+// a = b
+// b = c
 
 
-console.log("O novo valor de a é", a) // O novo valor de a é 25
-console.log("O novo valor de b é", b) // O novo valor de b é 10
+// console.log("O novo valor de a é", a) // O novo valor de a é 25
+// console.log("O novo valor de b é", b) // O novo valor de b é 10
+
 
