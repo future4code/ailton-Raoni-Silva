7a00e6fc1df012f753aa09ab79fb39422c937209
Raoni Lobo <teste@MacBook-Pro-de-Raoni.local>
2022-07-05 16:25:14 -0300

    atividade useEffect

--- modulo3/projeto-Astromatch/astromatch/src/App.js
+++ modulo3/projeto-Astromatch/astromatch/src/App.js
@@ -1,47 +1,27 @@
 import React from "react";
 import Home from "./Home/Home";
-import MatchScreen from "./MatchScreen.js/MatchScreen";
 import styled from "styled-components";
-import { useState } from "react";
 
 const ContainerAll = styled.div`
+ border: 1px solid black;
+  border-radius: 10px;
+  width: 25vw;
+  height: 80vh;
   display: flex;
   flex-direction: column;
-  background-color: black;
-  color: white;
-  flex-wrap: wrap;
   align-items: center;
-  justify-items: center;
-  justify-self: center;
-  width: 40%;
-  height: 60%;
-`;
-
+  background-color: black;
+  color:whitesmoke;
+  `;
 
 export default function App() {
-  const [tela, setTela] = useState("home");
 
- const atualizaTela = (id) => {
-   
-  };
-
-  const trocaTela = () => {
-    switch (tela) {
-      case "home":
-        return <Home />;
-      case "matchScreen":
-        return <MatchScreen />;
-      default:
-        return <Home />;
-    }
-  };
+  
 
   return (
     <ContainerAll>
-      <div>RENDERIZA</div>
-      <button onClick={trocaTela()}>Troca Tela</button>
-      <Home/>
-      <MatchScreen/>
-    </ContainerAll>
+      <h1>AstroMatch</h1>
+     <Home/>
+     </ContainerAll>
   );
 }
--- modulo3/projeto-Astromatch/astromatch/src/MatchScreen.js/MatchScreen.js
+++ modulo3/projeto-Astromatch/astromatch/src/MatchScreen.js/MatchScreen.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+
+`
 
 export default function MatchScreen() {
   return (
+      <Container>
     <div>MatchScreen</div>
+    </Container>
   )
 }
